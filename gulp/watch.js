7deab28eba6a17f0649d0cdf47c2de2d51523b02
Borys Ihnatyev <paur.link@gmail.com>
2015-11-13 01:02:46 +0200

    Use gulp-watch for watching

--- gulp/watch.js
+++ gulp/watch.js
@@ -1,11 +1,20 @@
 'use strict';
 
-const watchOptions = {debounceDelay: 1000};
+const watch = require('gulp-watch'),
+    watchOptions = {debounceDelay: 1000};
 
 gulp.task('watch', ['scripts:watch'], () => {
 
-    gulp.watch(config.paths.src.index, watchOptions, ['html']);
-    gulp.watch(config.patterns.src.templates, watchOptions, ['templates']);
-    gulp.watch(config.patterns.src.styles.all, watchOptions, ['styles:only']);
-
+    watch(config.paths.src.index, startTask('html'));
+    watch(config.patterns.src.templates, startTask('templates'));
+    watch(config.patterns.src.styles.all, startTask('styles:only'));
+    watch(config.patterns.src.media.images, startTask('images'));
+    watch(config.patterns.src.media.icons.all, startTask('icons'));
+    watch(config.patterns.src.media.fonts, startTask('fonts'));
 });
+
+function startTask(task) {
+    return () => {
+        gulp.start(task);
+    }
+}
