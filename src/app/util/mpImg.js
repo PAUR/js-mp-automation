dc2e8102a9ee660663f9961506275336091511dc
Borys Ihnatyev <paur.link@gmail.com>
2015-12-10 00:45:38 +0200

    Add alt attribute for img tags

--- src/app/util/mpImg.js
+++ src/app/util/mpImg.js
@@ -7,6 +7,7 @@ function mpImg() {
         link: (scope, element) => {
             const domElement = element[0];
             element.attr('src', `https://placekitten.com/g/${domElement.width}/${domElement.height}`);
+            element.attr('alt', 'placekitten');
         }
     };
 }
