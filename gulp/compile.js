76b5ae65002fad4271808f51bbf964e1cd2d6791
Borys Ihnatyev <paur.link@gmail.com>
2015-12-27 23:12:28 +0200

    fix(gulp:compile) scripts was declared twice

--- gulp/compile.js
+++ gulp/compile.js
@@ -3,7 +3,8 @@
 const runSequence = require('run-sequence');
 
 gulp.task('compile', done => {
-    let compileTasks = ['html', 'fonts', 'images', 'scripts', 'styles'];
+
+    let compileTasks = ['html', 'fonts', 'images', 'styles'];
 
     if (!env.isWatch) {
         compileTasks = compileTasks.concat('scripts')
