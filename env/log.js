b12249df206d26c5dc2adf35241dcb1b3d161eda
Borys Ihnatyev <paur.link@gmail.com>
2015-12-27 23:38:52 +0200

    fix(env/log) return fn value when fn has no args

--- env/log.js
+++ env/log.js
@@ -13,13 +13,16 @@ function log(env) {
 }
 
 function logVariable(env, name) {
-    Object.keys(env).forEach(function(variableName) {
+    Object.keys(env).forEach(function (variableName) {
 
         var value = env[variableName];
 
         if (typeof value === 'object') {
             logVariable(value, name + ' ' + gutil.colors.blue('[' + variableName + ']'));
         } else {
+            if (typeof value === 'function' && value.length === 0) {
+                value = env[variableName]();
+            }
             gutil.log(gutil.colors.green(name) + ' ' + gutil.colors.blue('[' + variableName + ']') + ' : ' + value);
         }
 
